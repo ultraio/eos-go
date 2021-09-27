03c935465766f5ad184056c103c364e92e54c575
Duncan Dam <duncan.dam@ultra.io>
2021-09-27 14:40:01 +0700

    [UB-1339] Add open token

---
+++ token/open.go
@@ -0,0 +1,25 @@
+package token
+
+import eos "github.com/eoscanada/eos-go"
+
+func NewOpen(owner eos.AccountName, symbol eos.Symbol, ram_payer eos.AccountName) *eos.Action {
+	return &eos.Action{
+		Account: AN("eosio.token"),
+		Name:    ActN("open"),
+		Authorization: []eos.PermissionLevel{
+			{Actor: to, Permission: PN("active")},
+		},
+		ActionData: eos.NewActionData(Issue{
+			Owner:		owner,
+			Symbol: 	symbol,
+			RamPayer:	ram_payer,
+		}),
+	}
+}
+
+// Open represents the `open` struct on the `eosio.token` contract.
+type Open struct {
+	Owner       eos.AccountName	`json:"owner"`
+	Symbol		eos.Symbol		`json:"symbol"`
+	RamPayer	eos.AccountName	`json:"ram_payer"`
+}
