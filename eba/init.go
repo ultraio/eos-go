49735633c0739c6a3c82b77f21709f27969e5a9c
Duncan Dam <duncan.dam@ultra.io>
2021-09-29 15:29:35 +0700

    [UB-1339] add missing defination

--- eba/init.go
+++ eba/init.go
@@ -5,3 +5,7 @@ import "github.com/ultraio/eos-go"
 func init() {
 	eos.RegisterAction(AN("eosio.eba"), ActN("regidp"), Regidp{})
 }
+
+var AN = eos.AN
+var PN = eos.PN
+var ActN = eos.ActN
--- oracle/init.go
+++ oracle/init.go
@@ -6,3 +6,7 @@ func init() {
 	eos.RegisterAction(AN("eosio.oracle"), ActN("init"), Init{})
 	eos.RegisterAction(AN("eosio.oracle"), ActN("regexchange"), Regexchange{})
 }
+
+var AN = eos.AN
+var PN = eos.PN
+var ActN = eos.ActN
